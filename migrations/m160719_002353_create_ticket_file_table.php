401c027c44448f61119119f3bf861375f25fb9d1
ricco381 <ricco381@gmail.com>
2016-07-19 03:30:17 +0300

    add upload images

---
+++ migrations/m160719_002353_create_ticket_file_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `ticket_file_table`.
+ */
+class m160719_002353_create_ticket_file_table extends Migration
+{
+
+    private $table = 'ticket_file';
+
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable($this->table, [
+            'id' => $this->primaryKey(),
+            'id_body' => $this->integer()->notNull(),
+            'fileName' => $this->string(255)->notNull(),
+        ]);
+        
+        $this->createIndex('i_id_body', $this->table, 'id_body');
+        $this->addForeignKey('fk_id_body', $this->table, 'id_body', 'ticket_body', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropForeignKey('fk_id_body', $this->table);
+        $this->dropTable('ticket_file_table');
+    }
+}
